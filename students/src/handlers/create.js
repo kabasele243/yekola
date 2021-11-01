3a5df92d4fab7c321d2d615a8832fb320c03f915
Franck Kabasele <kabasele467@gmail.com>
2021-11-01 19:37:47 -0400

    add dynamodb and s3

--- students/src/handlers/create.js
+++ students/src/handlers/create.js
@@ -1,11 +1,33 @@
+import { v4 as uuid } from 'uuid';
+import AWS from 'aws-sdk';
+import commonMiddleware from '../lib/commonMiddleware';
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+
 async function create(event, context) {
 
-const result = event.body;
-
-  return {
-    statusCode: 200,
-    body: result,
-  };
+    const { title } = event.body;
+    
+    const auction = {
+      id: uuid(),
+      title,
+      status: 'OPEN',
+    };
+  
+    try {
+      await dynamodb.put({
+        TableName: process.env.STUDENTS_TABLE_NAME,
+        Item: auction,
+      }).promise();
+    } catch(error) {
+      console.error(error);
+      throw new createError.InternalServerError(error);
+    }
+  
+    return {
+      statusCode: 201,
+      body: JSON.stringify(auction),
+    };
 }
 
-export const handler = create;
+export const handler = commonmiddleware(create);
